8800c5170380649646505c58fd60c125bed5db55
xiaomi4356 <99778109+xiaomi4356@users.noreply.github.com>
2023-04-20 21:19:05 +0800

    Add files via upload

---
+++ VGDAE_tnnls/hys/hyp_sens/hyp_sens_lambda.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib
+#[0.0001, 0.001, 0.005, 0.01, 0.03, 0.05, 0.07, 0.1, 0.2,0.3, 0.4, 0.45, 0.5, 0.55,0.57,0.59, 0.6,0.7,0.75,0.8,1]
+
+Cora_lambda=[0.951, 0.9562199122165852, 0.9581203259292332, 0.9494312801327913, 0.9564877992575497, 0.9494986119562595, 0.9522602969081371, 0.9560333994649459, 0.9518242603401157, 0.9555379524644527, 0.9555595562580788]
+Citeseer_lambda=[0.969, 0.9738727206859075, 0.9753759207825141, 0.9747711629030311, 0.9720458881777564, 0.9690385219176427, 0.9686144185484846, 0.9763748339572516, 0.9702586644125105, 0.9744040574809807, 0.9713174737350562]
+Pubmed_lambda=[0.967,0.9677385210855739, 0.9639840227049096, 0.9602818493089315, 0.9616156120485735, 0.9612570601565249, 0.9588336059377811, 0.9568182692902945, 0.9573794824968396, 0.9571621082804416, 0.9556282958936647]
+
+Cora_lambda=[i*100 for i in Cora_lambda]
+Citeseer_lambda=[i*100 for i in Citeseer_lambda]
+Pubmed_lambda=[i*100 for i in Pubmed_lambda]
+
+def visualize_lambda(list1, list2, list3):
+    plt.figure(dpi=600)
+    matplotlib.rcParams['font.size'] = 18
+
+    matplotlib.rcParams['font.family'] = 'Times New Roman'
+    #修改图中字体
+    # plt.rc('font', family='Times New Roman')
+    #修改字号
+    # plt.xticks(fontsize=9)
+    # plt.yticks(fontsize=9)
+    # plt.tick_params(labelsize=9)
+    #设置坐标轴名称
+    plt.xlabel('$\lambda _{mi}$')
+    plt.ylabel('AUC(%)')
+    x=np.linspace(0,1,11)
+    plt.xlim(-0.05, 1.05)
+    plt.ylim(93, 98)
+    plt.xticks(np.arange(0, 1.1, step=0.1))
+    plt.yticks(np.arange(93, 98.5, step=1))
+    plt.plot(x, list1, 'D', linestyle='-', color='#ff7f0e', markersize=6, linewidth=1.5, label='Cora')
+    plt.plot(x, list2, 'o', linestyle='-', color='#1f77b4', markersize=6,linewidth=1.5, label='Citeseer')
+    plt.plot(x, list3, '^', linestyle='-', color='#2ca02c', markersize=6, linewidth=1.5, label='Pubmed')
+
+    plt.grid(visible=True, linestyle = ':')
+    plt.legend(loc='lower right')
+    plt.savefig("lambda.eps", dpi=600,bbox_inches='tight', pad_inches=0.1, format='eps')
+    plt.savefig("lambda.png", dpi=600,bbox_inches='tight', pad_inches=0.1,)
+    # plt.show()
+
+visualize_lambda(Cora_lambda,Citeseer_lambda,Pubmed_lambda)
---
+++ VGDAE_tnnls/hys/hys_lambda/main/model.py
@@ -0,0 +1,76 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+
+# Feature disentangle layer
+class RoutingLayer(nn.Module):
+    def __init__(self, args):
+        super(RoutingLayer, self).__init__()
+        self.k = args.k
+        self.routit = args.routit
+
+    def forward(self, x, src_trg):
+        m, src, trg = src_trg.shape[1], src_trg[0], src_trg[1]
+        n, d = x.shape
+        k, delta_d = self.k, d // self.k
+
+        x = F.normalize(x.view(n, k, delta_d), dim=2).view(n, d)
+        z = x[src].view(m, k, delta_d)  # neighbors' feature
+        c = x  # node-neighbor attention aspect factor
+        for t in range(self.routit):
+            p = (z * c[trg].view(m, k, delta_d)).sum(dim=2)  # update node-neighbor attention aspect factor
+            p = F.softmax(p, dim=1)
+            p = p.view(-1, 1).repeat(1, delta_d).view(m, k, delta_d)
+
+            weight_sum = (p * z).view(m, d)  # weight sum (node attention * neighbors feature)
+            c = c.index_add_(0, trg, weight_sum)  # update output embedding
+
+            c = F.normalize(c.view(n, k, delta_d), dim=2).view(n, d) # embedding normalize aspect factor
+        return c
+
+class Disen_Linear(nn.Module):
+    def __init__(self, in_dim, args):
+        super(Disen_Linear, self).__init__()
+        self.linear = nn.Linear(in_dim, args.k*args.x_dim)
+        if args.model == 'VGAE':
+            self.linear_= nn.Linear(in_dim, args.k*args.x_dim)
+        self.routlay = RoutingLayer(args)
+        self.dropout = args.dropout
+
+
+    def forward(self, x, edge_index, args):
+        if args.model =='GAE':
+            x = self.linear(x)
+            x = self.routlay(x, edge_index)
+            # x = F.dropout(x, hyperpm['dropout'], training=self.training)
+            return x
+        if args.model =='VGAE':
+            mu = self.linear(x)
+            # mu = F.relu(mu)
+            mu = self.routlay(mu, edge_index)
+            # mu = F.dropout(mu, hyperpm['dropout'], training=self.training)
+
+            log = self.linear_(x)
+            # log = F.relu(log)
+            # log = self.routlay(log, edge_index)
+
+            return mu, log
+
+
+
+class max_deco(nn.Module):
+    def   __init__(self, hyperpm):
+        super(max_deco, self).__init__()
+        self.k = hyperpm['k']
+        self.d = hyperpm['out_dim']//hyperpm['k']
+
+    def forward(self, z, edge_index, sigmoid=True):
+        m= edge_index.size(1)
+        src = z[edge_index[0]].view(m, self.k, self.d)
+        trg = z[edge_index[1]].view(m, self.k, self.d)
+        p = (src*trg).sum(dim=2)
+        value = torch.max(p, dim=1, keepdim=False).values
+
+        return torch.sigmoid(value) if sigmoid else value
+
+
